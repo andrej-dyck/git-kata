ea4f3ad397b669539a9e73973f0cc45292a3bca8
Andrej Dyck <43913051+andrej-dyck@users.noreply.github.com>
2023-07-28 11:48:31 +0200

    Amend sed command to work on Mac OS X

--- scripts/file-functions.sh
+++ scripts/file-functions.sh
@@ -33,7 +33,7 @@ in-src() {
 replace-in-file() {
   local file="$1" searchStr="$2" replaceStr="$3"
 
-  sed -i "s/$searchStr/$replaceStr/" "$file"
+  sed -i '' "s/$searchStr/$replaceStr/" "$file"
 }
 
 insert-line-after-in-file() {
@@ -45,7 +45,7 @@ insert-line-after-in-file() {
 prepend-line-to-file() {
   local file="$1" newLine="$2"
 
-  sed -i '1s/^/'"$newLine"'\n/' "$file"
+  sed -i '' '1s/^/'"$newLine"'\n/' "$file"
 }
 
 extract-lines-to-new-file() {
@@ -58,8 +58,8 @@ remove-lines-in-file() {
   local file="$1" fromLine="$2" toLine="$3"
 
   if [ -z "$toLine" ]; then
-    sed -i "${fromLine}d" "$file"
+    sed -i '' "${fromLine}d" "$file"
   else
-    sed -i "${fromLine},${toLine}d" "$file"
+    sed -i '' "${fromLine},${toLine}d" "$file"
   fi
 }
